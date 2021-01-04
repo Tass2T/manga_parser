8cd86bc53f7c1cc586701ab131a771109bdc6f56
Tass2T <brunog90@gmail.com>
2021-01-04 15:42:20 +0100

    interface working (barely)

--- interface.py
+++ interface.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from mangaGetter import scrapManga
 
 interface = tk.Tk()
 # we change the title here
 interface.title("Pandaru Manga")
+
 # we change the min and max resolution and keep it from changing
 interface.minsize(640,480)
 interface.maxsize(1240,720)
@@ -11,15 +13,29 @@ interface.resizable(width=False, height=False)
 # below, we get the height and width of the window's screen
 screen_x = interface.winfo_screenwidth()
 screen_y = interface.winfo_screenheight()
-# then the app geometry
+# then the app width and height
 window_x = 800
 window_y = 600
 # posX and posY are screen split in two minus halft width and height of app
 posX = (screen_x // 2) - (window_x // 2)
 posY = (screen_y // 2) - (window_y // 2)
-
 geo = "{}x{}+{}+{}".format(window_x,window_y, posX, posY)
+# change app gemometry based on previous variables
 interface.geometry(geo)
+# first label creation then how to display it
+# pack display in block
+welcome_text = tk.Label(interface, text="Welcome to the Pandaru Manga Scrapper!")
+welcome_text.pack()
+# here we get the manga chosen by the user
+userInput = tk.Entry(interface)
+userInput.pack()
+
+def changeLabel () :
+    formattedName = tk.Entry.get(userInput).replace(' ', '-')
+    scrapManga(formattedName)
+
+myFirstButton = tk.Button(interface, text="change the label", command=changeLabel)
+myFirstButton.pack()
 
 # will create an infinite loop to keep the windows open
 interface.mainloop()
